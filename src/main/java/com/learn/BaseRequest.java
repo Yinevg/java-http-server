05f19163236d6bb62039219f461bc71f9a30ac57
Yinevg <864248425@qq.com>
2019-04-10 22:27:38 +0800

    add baserequest and log

---
+++ src/main/java/com/learn/BaseRequest.java
@@ -0,0 +1,50 @@
+package com.learn;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Yinevg on 2019/4/10
+ */
+public class BaseRequest {
+
+    private String url;
+    private String method;
+    // TODO: 2019/4/10 header待做
+
+//    http请求
+//    GET /index HTTP/1.1
+//    Host: localhost:8080
+//    Connection: keep-alive
+//    Accept: */*
+//    User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36
+
+    public BaseRequest(InputStream in) throws IOException {
+        String header = "";
+        byte[] buffer = new byte[1024];
+        int length = 0;
+        if ((length = in.read(buffer)) > 0) {
+            // TODO: 2019/4/10 暂定header长度小于1024比特
+            header = new String(buffer, 0, length);
+        }
+        String httpHead = header.split("\n")[0];
+        method = httpHead.split(" ")[0];
+        url = httpHead.split(" ")[1];
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
--- src/main/java/com/learn/Server.java
+++ src/main/java/com/learn/Server.java
@@ -1,7 +1,16 @@
 package com.learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Yinevg on 2019/4/10
  */
 public class Server {
+
+    private static Logger log = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        log.info("server start");
+    }
 }
